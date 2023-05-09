88aa6e753bf62f2ec9d7800487354e2397f16a9b
vincent <kratos_angel_59@hotmail.fr>
2023-05-09 18:37:35 +0200

    Add alerts succes+fail + error

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -12,13 +12,14 @@ class GossipsController < ApplicationController
   end
 
   def new
-
+    @gossip = Gossip.new()
   end
 
   def create
     @gossip = Gossip.new(title: params['title'], content: params['content'], user_id: User.all.sample.id)
 
     if @gossip.save
+      flash[:success] = "Le potin a bien été créé !"
       redirect_to '/'
     else
       render 'new'
